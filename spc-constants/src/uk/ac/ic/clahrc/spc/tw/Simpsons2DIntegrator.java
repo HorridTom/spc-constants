158356181645ab858aed28195fe3128f04dc89e9
HorridTom <woodcock.thomas@gmail.com>
2018-11-05 17:59:02 +0000

    Added implementation of 2D Simpsons rule.
    Also added argument checks to weight method.

--- spc-constants/src/uk/ac/ic/clahrc/spc/tw/Simpsons2DIntegrator.java
+++ spc-constants/src/uk/ac/ic/clahrc/spc/tw/Simpsons2DIntegrator.java
@@ -8,12 +8,48 @@ public class Simpsons2DIntegrator {
 	}
 
 	public static double integrate(Integrand integrand, double xLowLimit, double xHighLimit,
-			double yLowLimit, double yHighLimit, int nStrips, double... params) {
-		// TODO Auto-generated method stub
-		return 0;
+			double yLowLimit, double yHighLimit, int xStrips, int yStrips, double... params) {
+		
+		if (xStrips < 2 || xStrips % 2 == 1 || yStrips < 2 || yStrips % 2 == 1) {
+			throw new IllegalArgumentException(
+					"Illegal number of strips, must be positive even integer.");
+		}
+		if(xLowLimit >= xHighLimit || yLowLimit >= yHighLimit) {
+			throw new IllegalArgumentException(
+					"Invalid limits.");
+		}
+		
+		double xStripWidth = (xHighLimit - xLowLimit) / xStrips;
+		double yStripWidth = (yHighLimit - yLowLimit) / yStrips;
+		
+		double factor = (double) 1 / (9 * xStrips * yStrips);
+		
+		double sumTotal = 0;
+		
+		for (int s = 0; s <= xStrips ; s++) {
+			for (int t = 0; t <= yStrips ; t++) {
+				double x = xLowLimit + s * xStripWidth;
+				double y = yLowLimit + t * yStripWidth;
+				sumTotal += factor * Simpsons2DIntegrator.weight(s, t, xStrips, yStrips) *
+						integrand.func(x, y, params);
+			}
+		}
+		
+		return sumTotal;
 	}
 
-	public static Object weight(int i, int j, int xStrips, int yStrips) {
+	
+	public static int weight(int i, int j, int xStrips, int yStrips) {
+		
+		if (xStrips < 2 || xStrips % 2 == 1 || yStrips < 2 || yStrips % 2 == 1) {
+			throw new IllegalArgumentException(
+					"Illegal number of strips, must be positive even integer.");
+		}
+		
+		if(i < 0 || i > xStrips || j < 0 || j > yStrips) {
+			throw new IllegalArgumentException("Weight not defined.");
+		}
+		
 		if ((i == 0 || i == xStrips) && (j == 0 || j == yStrips)) {
 			return 1;
 		} else if ((i == 0 || i == xStrips) ^ (j == 0 || j == yStrips)) {
