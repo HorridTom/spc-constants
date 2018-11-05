3d496d277ba0099cb2e19dc05636705eb062f6fa
HorridTom <woodcock.thomas@gmail.com>
2018-11-05 18:51:04 +0000

    Added implementation of d3 calculation.

--- spc-constants/src/uk/ac/ic/clahrc/spc/tw/SPCConstants.java
+++ spc-constants/src/uk/ac/ic/clahrc/spc/tw/SPCConstants.java
@@ -15,8 +15,15 @@ public class SPCConstants {
 	}
 
 	public static double d3(int i) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		double d3Integral = Simpsons2DIntegrator.integrate(new d3TransformedIntegrand(),
+				-10, 10, 0, 10, 50, 50, i);
+		
+		double term2 = Math.pow(d2(i),2);
+		
+		double d3Result = Math.sqrt(2 * d3Integral - term2);
+		
+		return d3Result;
 	}
 	
 	public static class d2TransformedIntegrand implements Simpsons1DIntegrator.Integrand {
@@ -29,6 +36,24 @@ public class SPCConstants {
 				double funcValue = (1 - Math.pow(1 - p1, params[0]) - Math.pow(p1, params[0])) / Math.pow(xVar, 2);
 				return funcValue;
 			}
+		}
+	}
+	
+	public static class d3TransformedIntegrand implements Simpsons2DIntegrator.Integrand {
+		public double func(double xVar, double yVar, double... params) {
+			
+			NormalDistribution nDist = new NormalDistribution();
+			
+			double x = (xVar + yVar) / 2;
+			double y = (xVar - yVar) / 2;
+			
+			double PhiX = nDist.cumulativeProbability(x);
+			double PhiY = nDist.cumulativeProbability(y);
+			
+			double d3integrand = 1 - Math.pow(PhiX, params[0]) - Math.pow(1 - PhiY, params[0]) +
+					Math.pow(PhiX - PhiY, params[0]);
+			
+			return (double)0.5 * d3integrand;
 		}
 	}
 	
