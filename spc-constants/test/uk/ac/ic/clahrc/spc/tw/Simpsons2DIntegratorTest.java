6d52ca669b4ce705e91921f7763832eddffecdab
HorridTom <woodcock.thomas@gmail.com>
2018-11-05 17:30:12 +0000

    Test for 2D weights method.
    Also corrected mistake specifying constant doubles.

--- spc-constants/test/uk/ac/ic/clahrc/spc/tw/Simpsons2DIntegratorTest.java
+++ spc-constants/test/uk/ac/ic/clahrc/spc/tw/Simpsons2DIntegratorTest.java
@@ -8,8 +8,8 @@ public class Simpsons2DIntegratorTest {
 
 	@Test
 	public void testSimpsons2DIntegrator() {
-		double exact_integral_1 = 1/9;
-		double correct_answer_integral_2 = 1.1351*2;
+		double exact_integral_1 = (double) 1/9;
+		double correct_answer_integral_2 = (double) 1.1351*2;
 		
 		double simpsons_integral_1 = Simpsons2DIntegrator.integrate(new sqrIntegrand(), 0, 1, 0, 1, 1000);
 		double simpsons_integral_2 = Simpsons2DIntegrator.integrate(
@@ -17,6 +17,56 @@ public class Simpsons2DIntegratorTest {
 		
 		assertEquals(exact_integral_1, simpsons_integral_1, 0.0001);
 		assertEquals(correct_answer_integral_2, simpsons_integral_2, 0.0001);
+	}
+	
+	@Test
+	public void testWeights() {
+		// Check weights are correct for two by two strips
+		assertEquals(1, Simpsons2DIntegrator.weight(0, 0, 2, 2));
+		assertEquals(4, Simpsons2DIntegrator.weight(1, 0, 2, 2));
+		assertEquals(1, Simpsons2DIntegrator.weight(2, 0, 2, 2));
+		assertEquals(4, Simpsons2DIntegrator.weight(0, 1, 2, 2));
+		assertEquals(16, Simpsons2DIntegrator.weight(1, 1, 2, 2));
+		assertEquals(4, Simpsons2DIntegrator.weight(2, 1, 2, 2));
+		assertEquals(1, Simpsons2DIntegrator.weight(0, 2, 2, 2));
+		assertEquals(4, Simpsons2DIntegrator.weight(1, 2, 2, 2));
+		assertEquals(1, Simpsons2DIntegrator.weight(2, 2, 2, 2));
+		
+		// Check weights are correct for four by four strips
+		assertEquals(1, Simpsons2DIntegrator.weight(0, 0, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(1, 0, 4, 4));
+		assertEquals(2, Simpsons2DIntegrator.weight(2, 0, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(3, 0, 4, 4));
+		assertEquals(1, Simpsons2DIntegrator.weight(4, 0, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(0, 1, 4, 4));
+		assertEquals(16, Simpsons2DIntegrator.weight(1, 1, 4, 4));
+		assertEquals(8, Simpsons2DIntegrator.weight(2, 1, 4, 4));
+		assertEquals(16, Simpsons2DIntegrator.weight(3, 1, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(4, 1, 4, 4));
+		assertEquals(2, Simpsons2DIntegrator.weight(0, 2, 4, 4));
+		assertEquals(8, Simpsons2DIntegrator.weight(1, 2, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(2, 2, 4, 4));
+		assertEquals(8, Simpsons2DIntegrator.weight(3, 2, 4, 4));
+		assertEquals(2, Simpsons2DIntegrator.weight(4, 2, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(0, 3, 4, 4));
+		assertEquals(16, Simpsons2DIntegrator.weight(1, 3, 4, 4));
+		assertEquals(8, Simpsons2DIntegrator.weight(2, 3, 4, 4));
+		assertEquals(16, Simpsons2DIntegrator.weight(3, 3, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(4, 3, 4, 4));
+		assertEquals(1, Simpsons2DIntegrator.weight(0, 4, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(1, 4, 4, 4));
+		assertEquals(2, Simpsons2DIntegrator.weight(2, 4, 4, 4));
+		assertEquals(4, Simpsons2DIntegrator.weight(3, 4, 4, 4));
+		assertEquals(1, Simpsons2DIntegrator.weight(4, 4, 4, 4));
+	
+		
+		// Check some weights correct for 1000 by 1000 strips
+		assertEquals(16, Simpsons2DIntegrator.weight(203, 105, 1000, 1000));
+		assertEquals(4, Simpsons2DIntegrator.weight(554, 896, 1000, 1000));
+		assertEquals(4, Simpsons2DIntegrator.weight(0, 105, 1000, 1000));
+		assertEquals(2, Simpsons2DIntegrator.weight(554, 0, 1000, 1000));
+		
+		
 	}
 	
 	public class sqrIntegrand implements Simpsons2DIntegrator.Integrand {
