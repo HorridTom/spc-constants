2c0f5c6446c1fa9910836f5d68d8ebad2a93f8cd
HorridTom <woodcock.thomas@gmail.com>
2018-11-05 15:50:57 +0000

    Corrected tolerance for specified significant figures.
    Since I'm only specifying the expected answers to 3 decimal places, no
    point asking for tolerance of 0.0001!

--- spc-constants/test/uk/ac/ic/clahrc/spc/tw/SPCConstantsTest.java
+++ spc-constants/test/uk/ac/ic/clahrc/spc/tw/SPCConstantsTest.java
@@ -22,10 +22,10 @@ public class SPCConstantsTest {
 		double d2_n_10 = SPCConstants.d2(10);
 		double d2_n_100 = SPCConstants.d2(100);
 		
-		assertEquals(1.128, d2_n_2, 0.0001);
-		assertEquals(1.693, d2_n_3, 0.0001);
-		assertEquals(3.078, d2_n_10, 0.0001);
-		assertEquals(5.015, d2_n_100, 0.0001);
+		assertEquals((double)1.128, d2_n_2, 0.001);
+		assertEquals((double)1.693, d2_n_3, 0.001);
+		assertEquals((double)3.078, d2_n_10, 0.001);
+		assertEquals((double)5.015, d2_n_100, 0.001);
 	}
 	
 	@Test
